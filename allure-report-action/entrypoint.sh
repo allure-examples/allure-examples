0ec12d9af37a3c2ce023ed40f1d11446299aeb4e
Aaftakhov <simple1elf@gmail.com>
2020-08-09 21:04:12 +0500

    check 1

--- allure-report-action/entrypoint.sh
+++ allure-report-action/entrypoint.sh
@@ -2,10 +2,15 @@
 
 unset JAVA_HOME
 
+mkdir -p ${INPUT_GH_PAGES}
 mkdir -p ${INPUT_ALLURE_HISTORY}
-echo "GITHUB_PAGES_URL ${INPUT_GITHUB_PAGES_URL}"
-echo "github build number ${{ github.run_number }}"
-echo "version ${INPUT_ALLURE_VERSION}"
+cp -r $(pwd)/${INPUT_GH_PAGES}/. $(pwd)/${INPUT_ALLURE_HISTORY}
+REPOSITORY_OWNER_SLASH_NAME=${INPUT_GITHUB_REPO}
+REPOSITORY_NAME=${REPOSITORY_OWNER_SLASH_NAME##*/}
+GITHUB_PAGES_WEBSITE_URL="https://${INPUT_GITHUB_REPO_OWNER}.github.io/${REPOSITORY_NAME}"
+echo "Github pages url $GITHUB_PAGES_WEBSITE_URL"
+
+#echo "version ${INPUT_ALLURE_VERSION}"
 
 
 echo "generating report from ${INPUT_ALLURE_RESULTS} to ${INPUT_ALLURE_REPORT} ..."
