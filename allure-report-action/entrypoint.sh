ec96804f472b6c0491f8ba95fb39032fbb03b3fb
Aaftakhov <simple1elf@gmail.com>
2020-08-09 17:42:37 +0500

    github context parse test

--- allure-report-action/entrypoint.sh
+++ allure-report-action/entrypoint.sh
@@ -1,26 +1,13 @@
 #! /usr/bin/env bash
 
-#ls -l
-#cd /github/workspace
-#echo "listing files in workspace ..."
-#ls -l
-
-#echo "generating report ..."
-#RESULTS_DIRECTORY="$1"
-#REPORT_DIRECTORY="$2"
-#RESULTS_HISTORY=$RESULTS_DIRECTORY/history
-#REPORT_HISTORY=$REPORT_DIRECTORY/history
-
-#if [ ! -d "$REPORT_DIRECTORY" ]; then
-#  mkdir $REPORT_DIRECTORY
-#fi
-
-#if [ -d "$RESULTS_HISTORY" ]; then
-#  echo "copying $RESULTS_HISTORY to $REPORT_HISTORY ..."
-#  cp -r $RESULTS_HISTORY $REPORT_HISTORY
-#fi
-
 unset JAVA_HOME
+
+mkdir -p ${INPUT_ALLURE_HISTORY}
+echo "GITHUB_PAGES_URL ${INPUT_GITHUB_PAGES_URL}"
+echo "github build number ${{ github.run_number }}"
+echo "version ${INPUT_ALLURE_VERSION}"
+
+
 echo "generating report from ${INPUT_ALLURE_RESULTS} to ${INPUT_ALLURE_REPORT} ..."
 #ls -l ${INPUT_ALLURE_RESULTS}
 allure generate --clean ${INPUT_ALLURE_RESULTS} -o ${INPUT_ALLURE_REPORT}
