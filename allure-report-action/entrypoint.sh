07f75e7572ed3a3ed5c0c9880747b451a3d364b2
Aaftakhov <simple1elf@gmail.com>
2020-08-09 17:25:14 +0500

    composite action test

--- allure-report-action/entrypoint.sh
+++ allure-report-action/entrypoint.sh
@@ -22,7 +22,7 @@
 
 unset JAVA_HOME
 echo "generating report from ${INPUT_ALLURE_RESULTS} to ${INPUT_ALLURE_REPORT} ..."
-ls -l ${INPUT_ALLURE_RESULTS}
+#ls -l ${INPUT_ALLURE_RESULTS}
 allure generate --clean ${INPUT_ALLURE_RESULTS} -o ${INPUT_ALLURE_REPORT}
 echo "listing report directory ..."
 ls -l ${INPUT_ALLURE_REPORT}
