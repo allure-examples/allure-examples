3bfa81e35b359ce22a19fa0b175a6474aac23e39
Aaftakhov <simple1elf@gmail.com>
2020-08-09 22:13:06 +0500

    final try

--- allure-report-action/entrypoint.sh
+++ allure-report-action/entrypoint.sh
@@ -5,27 +5,29 @@ unset JAVA_HOME
 mkdir -p ./${INPUT_GH_PAGES}
 mkdir -p ./${INPUT_ALLURE_HISTORY}
 cp -r ./${INPUT_GH_PAGES}/. ./${INPUT_ALLURE_HISTORY}
+
 REPOSITORY_OWNER_SLASH_NAME=${INPUT_GITHUB_REPO}
 REPOSITORY_NAME=${REPOSITORY_OWNER_SLASH_NAME##*/}
 GITHUB_PAGES_WEBSITE_URL="https://${INPUT_GITHUB_REPO_OWNER}.github.io/${REPOSITORY_NAME}"
 #echo "Github pages url $GITHUB_PAGES_WEBSITE_URL"
 
-# index.html
+echo "index.html"
 echo "<!DOCTYPE html><meta charset=\"utf-8\"><meta http-equiv=\"refresh\" content=\"0; URL=${GITHUB_PAGES_WEBSITE_URL}/${INPUT_GITHUB_RUN_NUM}\">" > index.html
 cat index.html
 mv ./index.html ./${INPUT_ALLURE_HISTORY}
+mv ./index2.html ./${INPUT_ALLURE_HISTORY}
 
-# executor.json
+echo "executor.json"
 echo '{"name":"GitHub Actions","type":"github","reportName":"Allure Report with history",' > executor.json
-echo '"url":"${GITHUB_PAGES_WEBSITE_URL}",' >> executor.json
-echo '"buildName":"GitHub Actions Run #${INPUT_GITHUB_RUN_ID}","buildOrder":"${INPUT_GITHUB_RUN_NUM}",' >> executor.json
+echo "\"url\":\"${GITHUB_PAGES_WEBSITE_URL}\"," >> executor.json # ???
 echo "\"reportUrl\":\"${GITHUB_PAGES_WEBSITE_URL}/${INPUT_GITHUB_RUN_NUM}\"," >> executor.json
-echo '"buildUrl":"https://github.com/${INPUT_GITHUB_REPO}/actions/runs/${INPUT_GITHUB_RUN_ID}"}' >> executor.json
+echo "\"buildUrl\":\"https://github.com/${INPUT_GITHUB_REPO}/actions/runs/${INPUT_GITHUB_RUN_ID}\"}" >> executor.json
+echo "\"buildName\":\"GitHub Actions Run #${INPUT_GITHUB_RUN_ID}\",\"buildOrder\":\"${INPUT_GITHUB_RUN_NUM}\"," >> executor.json
 cat executor.json
 mv ./executor.json ./${INPUT_ALLURE_RESULTS}
 
-# keep allure history
-cp -r ./${INPUT_GH_PAGES}/history/. ./${INPUT_ALLURE_RESULTS}/history
+echo "keep allure history"
+cp -r ./${INPUT_GH_PAGES}/last-history/. ./${INPUT_ALLURE_RESULTS}/history
 
 #echo "version ${INPUT_ALLURE_VERSION}"
 
@@ -35,4 +37,4 @@ allure generate --clean ${INPUT_ALLURE_RESULTS} -o ${INPUT_ALLURE_REPORT}
 #echo "listing report directory ..."
 #ls -l ${INPUT_ALLURE_REPORT}
 cp -r ./${INPUT_ALLURE_REPORT}/. ./${INPUT_ALLURE_HISTORY}/${INPUT_GITHUB_BUILD_NUM}
-cp -r ./${INPUT_ALLURE_REPORT}/history/. ./${INPUT_ALLURE_HISTORY}/history
+cp -r ./${INPUT_ALLURE_REPORT}/history/. ./${INPUT_ALLURE_HISTORY}/last-history
