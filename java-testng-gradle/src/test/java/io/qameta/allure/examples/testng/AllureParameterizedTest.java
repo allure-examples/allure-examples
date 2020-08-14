0f0e17975f754290d7540d1b21aef317feb0da3b
Aaftakhov <simple1elf@gmail.com>
2020-08-14 12:21:22 +0500

    java-testng-gradle

---
+++ java-testng-gradle/src/test/java/io/qameta/allure/examples/testng/AllureParameterizedTest.java
@@ -0,0 +1,36 @@
+package io.qameta.allure.examples.testng;
+
+import io.qameta.allure.Description;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import static io.qameta.allure.Allure.parameter;
+import static io.qameta.allure.Allure.step;
+
+public class AllureParameterizedTest {
+
+    //@ValueSource(strings = {"First Name", "Second Name"})
+    @DataProvider(name = "data-provider")
+    public Object[][] dataProviderMethod() {
+        return new Object[][] { {"First Name"}, {"Second Name"} };
+    }
+
+    //@ParameterizedTest(name = "{displayName} [{argumentsWithNames}]")
+
+    @Test(description = "allureParameterizedTest displayName", dataProvider = "data-provider")
+    @Parameters({"testParam"})
+    @Description("allureParameterizedTest description")
+    public void allureParameterizedTest(String testParam) {
+        //parameter("testParam", testParam);
+        step("Step inside parameterized test");
+        step("Test parameter: " + testParam);
+    }
+
+    @Test
+    public void allureFakeParameterizedTest() {
+        parameter("fakeParam","fakeValue");
+        step("Step inside fake parameterized test");
+    }
+
+}
