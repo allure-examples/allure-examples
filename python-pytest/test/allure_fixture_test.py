3661ed8931cfcfa6df6d71c99df0e2eeb050bd64
Stas Seliverstov <s.seliverstov@gmail.com>
2020-08-20 23:56:32 +0300

    add tests

---
+++ python-pytest/test/allure_fixture_test.py
@@ -0,0 +1,34 @@
+import allure
+import pytest
+
+@pytest.fixture(scope="session")
+def session_level_fixture(request):
+    with allure.step("Step inside session level fixture"):
+        pass
+
+    def finalizer():
+        with allure.step("Step inside finalizer session level fixture"):
+            pass
+
+    request.addfinalizer(finalizer)
+
+
+@pytest.fixture
+def function_level_fixture(request):
+    with allure.step("Step inside function level fixture"):
+        pass
+
+    def finalizer():
+        with allure.step("Step inside finalizer function level fixture"):
+            pass
+
+    request.addfinalizer(finalizer)
+
+
+def test_allure_fixture_test1(session_level_fixture, function_level_fixture):
+    with allure.step("Step inside test_allure_fixture_test1"):
+        pass
+
+def test_allure_fixture_test2(session_level_fixture, function_level_fixture):
+    with allure.step("Step inside test_allure_fixture_test2"):
+        pass
--- python-pytest/test/allure_simple_test.py
+++ python-pytest/test/allure_simple_test.py
@@ -1,2 +1,21 @@
+import allure
+
+@allure.title("test_allure_simple_test displayName")
 def test_allure_simple_test():
-    pass
+    with allure.step("Simple step"):
+        pass
+
+    with allure.step("Simple complex step"):
+        with allure.step("Simple nested step"):
+            pass
+
+    simple_step_func("function parameter")
+
+
+@allure.step("Simple function with step decorator")
+def simple_step_func(param):
+    with allure.step("function parameter: {param}".format(param=param)):
+        pass
+
+    with allure.step("Simple step inside test function"):
+        pass
