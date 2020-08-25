a4b96ef2dc77b804383cea078540d463c40fff0f
Aaftakhov <simple1elf@gmail.com>
2020-08-25 18:46:34 +0500

    gradle build files

---
+++ allure-junit4/build.gradle.kts
@@ -0,0 +1,58 @@
+import io.qameta.allure.gradle.AllureExtension
+
+plugins {
+    java
+    maven
+    id("io.qameta.allure") version "2.8.1"
+}
+
+group "io.qameta.allure.examples"
+version 1.3
+
+val allureVersion = "2.13.5"
+val junit4Version = "4.12"
+
+tasks.withType(JavaCompile::class) {
+    sourceCompatibility = "${JavaVersion.VERSION_1_8}"
+    targetCompatibility = "${JavaVersion.VERSION_1_8}"
+}
+
+tasks {
+    compileJava {
+        options.encoding = "UTF-8"
+    }
+    compileTestJava {
+        options.encoding = "UTF-8"
+    }
+}
+
+configure<AllureExtension> {
+    autoconfigure = true
+    aspectjweaver = true
+    version = allureVersion
+
+    clean = true
+
+    useJUnit4 {
+        version = allureVersion
+    }
+}
+
+tasks.withType(Test::class) {
+    ignoreFailures = true
+    useJUnit {
+
+    }
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    testImplementation("io.qameta.allure:allure-java-commons:$allureVersion")
+
+    testImplementation("junit:junit:$junit4Version")
+    testImplementation("org.slf4j:slf4j-simple:1.7.30")
+}
