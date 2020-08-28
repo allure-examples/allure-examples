2609ceb650c8ca40e5a2f802a714f7bf86d6bb3b
Aaftakhov <simple1elf@gmail.com>
2020-08-28 10:15:06 +0500

    add cucumber5-testng tests and features

---
+++ allure-cucumber5-testng/src/test/java/io/qameta/allure/examples/cucumber5/CucumberTest.java
@@ -0,0 +1,16 @@
+package io.qameta.allure.examples.cucumber5;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(
+        features = "src/test/resources/features",
+        plugin = {
+            "io.qameta.allure.cucumber5jvm.AllureCucumber5Jvm",
+            "progress",
+            "summary"
+        }
+)
+public class CucumberTest extends AbstractTestNGCucumberTests {
+
+}
